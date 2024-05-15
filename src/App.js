df1396f31a6ebdf989cbb9d150dc4604b2d7ba33
Nguyen Duc <DucNM.B21CN249@stu.ptit.edu.vn>
2024-05-15 20:23:49 +0700

    add gg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// App.js
+// src/App.js
 import './App.css';
 
 import React, { useState } from "react";
@@ -19,52 +19,52 @@ import { path } from './path';
 const App = (props) => {
   const [auth, setAuth] = useState({
     loggedIn: !!localStorage.getItem('token'),
-    user: null
+    user: JSON.parse(localStorage.getItem('user'))
   });
   const [users, setUsers] = useState([]);
 
-const updateUsers = async () => {
-  const updatedUsers = await fetchModel(`${path}user/list`);
-  setUsers(updatedUsers);
-}
+  const updateUsers = async () => {
+    const updatedUsers = await fetchModel(`${path}user/list`);
+    setUsers(updatedUsers);
+  };
+
   return (
-    <Router>
-      <div>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TopBar auth={auth} setAuth={setAuth} />
+      <Router>
+        <div>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TopBar auth={auth} setAuth={setAuth} />
+            </Grid>
+            <div className="main-topbar-buffer" />
+            <Grid item sm={3}>
+              <Paper className="main-grid-item">
+                {auth.loggedIn ? <UserList users={users} updateUsers={updateUsers} /> : <Typography>Please log in to see the user list.</Typography>}
+              </Paper>
+            </Grid>
+            <Grid item sm={9}>
+              <Paper className="main-grid-item">
+                <Routes>
+                  {auth.loggedIn ? (
+                      <>
+                        <Route path="/profile/:userId" element={<Profile auth={auth} setAuth={setAuth} updateUsers={updateUsers} />} />
+                        <Route path="/users/:userId" element={<UserDetail />} />
+                        <Route path="/photos/:userId" element={<UserPhotos />} />
+                        <Route path="/upload-photo" element={<UploadPhoto />} />
+                        <Route path="/users" element={<UserList />} />
+                      </>
+                  ) : (
+                      <>
+                        <Route path="/login" element={<Login setAuth={setAuth} />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="*" element={<Navigate to="/login" />} />
+                      </>
+                  )}
+                </Routes>
+              </Paper>
+            </Grid>
           </Grid>
-          <div className="main-topbar-buffer" />
-          <Grid item sm={3}>
-            <Paper className="main-grid-item">
-              {auth.loggedIn ? <UserList users={users} updateUsers={updateUsers}  /> : <Typography>Please log in to see the user list.</Typography>}
-            </Paper>
-          </Grid>
-          <Grid item sm={9}>
-            <Paper className="main-grid-item">
-              <Routes>
-                {auth.loggedIn ? (
-                  <>
-                   <Route path="/profile/:userId" element={<Profile auth={auth} setAuth={setAuth} updateUsers={updateUsers} />} />
-                    <Route path="/users/:userId" element={<UserDetail />} />
-                    <Route path="/photos/:userId" element={<UserPhotos />} />
-                    <Route path="/upload-photo" element={<UploadPhoto />} />  
-                    <Route path="/users" element={<UserList />} />
-                  </>
-                ) : (
-                  <>
-                    <Route path="/login" element={<Login setAuth={setAuth} />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="*" element={<Navigate to="/login" />} />
-                    <Route path="/register" element={<Register />} />
-                  </>
-                )}
-              </Routes>
-            </Paper>
-          </Grid>
-        </Grid>
-      </div>
-    </Router>
+        </div>
+      </Router>
   );
 };
 
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,11 @@
-// Login.js
+// src/components/Login.js
 import React, { useState } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { auth, googleProvider } from '../../auth/firebaseConfig';
+import { signInWithPopup } from "firebase/auth";
 import { path } from '../../path';
+import './../../App.css'; // Import CSS
 
 function Login({ setAuth }) {
   const [loginName, setLoginName] = useState('');
@@ -22,11 +25,21 @@ function Login({ setAuth }) {
       localStorage.setItem('token', data.token);
       localStorage.setItem('user', JSON.stringify(data.user));
       setAuth({ loggedIn: true, user: data.user });
-      // add name to alert
-      // alert(`Login successful ${data.user.first_name}!`)
-      // console.log(user);
       navigate(`/users/${data.user._id}`);
-      // alert(data.error);
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      const user = result.user;
+      const token = await user.getIdToken();
+      localStorage.setItem('token', token);
+      localStorage.setItem('user', JSON.stringify(user));
+      setAuth({ loggedIn: true, user });
+      navigate(`/users/${user.uid}`);
+    } catch (error) {
+      console.error("Error logging in with Google", error);
     }
   };
 
@@ -35,23 +48,25 @@ function Login({ setAuth }) {
   };
 
   return (
-      <Container>
+      <Container className="container">
         <h3>Login</h3>
         <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="loginName">
+          <Form.Group controlId="loginName" className="form-group">
             <Form.Label>Login Name</Form.Label>
             <Form.Control type="text" placeholder="Enter login name" value={loginName} onChange={e => setLoginName(e.target.value)} />
           </Form.Group>
-          <Form.Group controlId="password">
+          <Form.Group controlId="password" className="form-group">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} />
           </Form.Group>
-          <Button variant="primary" type="submit">Login</Button>
+          <Button variant="primary" type="submit" className="primary">Login</Button>
         </Form>
-        <div className="mt-3">
-          <Button variant="secondary" onClick={handleRegisterRedirect}>
-            Register
-          </Button>
+        <div className="button-group">
+          <div className="google-btn" onClick={handleGoogleLogin}>
+            {/*<img src="/images/g-logo.png" alt="Google logo" />*/}
+            <span>Login with Google</span>
+          </div>
+          <Button variant="secondary" onClick={handleRegisterRedirect} className="secondary">Register</Button>
         </div>
       </Container>
   );
